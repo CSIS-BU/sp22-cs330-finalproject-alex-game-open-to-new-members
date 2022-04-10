abefa25bff0e32ce77027a267b92b128b38126be
ahoerr2 <98572749+amhoerr2@users.noreply.github.com>
2022-04-10 17:53:04 -0500

    Refined Python Skeleton Code

--- client_server-py/hangman-client.py
+++ client_server-py/hangman-client.py
@@ -1,31 +1,40 @@
-import sys 
-import socket 
- 
-SEND_BUFFER_SIZE = 2048 
- 
-def client(server_ip, server_port): 
-    """TODO: Open socket and send message from sys.stdin""" 
-    # create an INET, STREAMing socket 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
-        # now connect to server 
-        s.connect((server_ip, server_port)) 
-         
-        while True: 
-            content = sys.stdin.buffer.read(SEND_BUFFER_SIZE) 
-            if not content: break 
-            sent = s.sendall(content) 
-            if sent == 0: 
-                raise RuntimeError("socket connection broken") 
-    pass 
- 
- 
-def main(): 
-    """Parse command-line arguments and call client function """ 
-    if len(sys.argv) != 3: 
-        sys.exit("Usage: python3 client-python.py [Server IP] [Server Port] < [message]") 
-    server_ip = sys.argv[1] 
-    server_port = int(sys.argv[2]) 
-    client(server_ip, server_port) 
- 
-if __name__ == "__main__": 
-    main() 
+###############################################################################
+# client-python.py
+# Name: Alex Hoerr
+###############################################################################
+
+import sys
+import socket
+import random
+import string
+
+# Size of chunks the client sends messages to the server with
+SEND_BUFFER_SIZE = 2048
+# client(): Open socket and send message from sys.stdin
+def client(server_ip, server_port):
+
+    # Opens a socket and sets the socket mode to IPV4 and TCP
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+
+        # Connects to the server using the ip and port the user specified
+        sock.connect((server_ip, server_port))
+
+        # WHile a connection is open send all of the data rom the input stream to the server in chunks the size of SEND_BUFFER_SIZE
+        with open(0,'rb'):
+            while True:
+                msg = sys.stdin.buffer.raw.read(SEND_BUFFER_SIZE)
+                if not msg: 
+                    return
+                sendmsg = sock.sendall(msg)
+
+
+def main():
+    """Parse command-line arguments and call client function """
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python3 client-python.py [Server IP] [Server Port] < [message]")
+    server_ip = sys.argv[1]
+    server_port = int(sys.argv[2])
+    client(server_ip, server_port)
+
+if __name__ == "__main__":
+    main()
--- client_server-py/hangman-server.py
+++ client_server-py/hangman-server.py
@@ -1,38 +1,48 @@
-import sys 
-import socket 
- 
-RECV_BUFFER_SIZE = 2048 
-QUEUE_LENGTH = 10 
- 
-def server(server_port): 
-    """TODO: Listen on socket and print received message to sys.stdout""" 
-    # create an INET, STREAMing socket 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket: 
-        # bind the socket to the host and its port 
-        serversocket.bind(('', server_port)) 
-        # prepare for connection 
-        serversocket.listen(QUEUE_LENGTH) 
- 
-        while True: 
-            # accept connections from outside 
-            (clientsocket, address) = serversocket.accept() 
- 
-            with clientsocket: 
-                while True: 
-                    # receive data and print it out 
-                    data = clientsocket.recv(RECV_BUFFER_SIZE) 
-                    if not data: break 
-                    sys.stdout.buffer.write(data) 
-                sys.stdout.flush() 
-    pass 
- 
- 
-def main(): 
-    """Parse command-line argument and call server function """ 
-    if len(sys.argv) != 2: 
-        sys.exit("Usage: python server-python.py [Server Port]") 
-    server_port = int(sys.argv[1]) 
-    server(server_port) 
- 
-if __name__ == "__main__": 
-    main() 
+#!/usr/bin/env python
+###############################################################################
+# server-python.py
+# Name: Alex Hoerr
+###############################################################################
+
+import sys
+import socket
+
+RECV_BUFFER_SIZE = 2048
+QUEUE_LENGTH = 10
+
+# server() Listen on socket and print received message to sys.stdout
+def server(server_port):
+
+    # These variables denote the host (localhost) to bind itself to the server and the user passed in port
+    HOST = "127.0.0.1" 
+    PORT = server_port
+
+    # Opens a socket and sets the socket mode to IPV4 and TCP
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+
+        # Binds the socket to the server
+        sock.bind((HOST,PORT))
+
+        # Listens for new clients with a client queue defined by QUEUE_LENGTH
+        sock.listen(QUEUE_LENGTH)
+
+        # Loops until a connection is accepted
+        # When a new connection is accpeted it loops through and recieves all the chunks of data the client sends to the server and then closes the connection
+        while True:
+            connection, address = sock.accept()
+            with connection:
+                while True:
+                    data = connection.recv(RECV_BUFFER_SIZE)
+                    if not data: break
+                    sys.stdout.buffer.raw.write(data)
+                sys.stdout.flush()
+
+def main():
+    """Parse command-line argument and call server function """
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python server-python.py [Server Port]")
+    server_port = int(sys.argv[1])
+    server(server_port)
+
+if __name__ == "__main__":
+    main()
