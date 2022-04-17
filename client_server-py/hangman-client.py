957cf3918981fec5dabcdf22ed2c3603397739c1
srushti-desai <98930189+srushti-desai@users.noreply.github.com>
2022-04-16 21:49:59 -0500

    Receive data & some of the game

--- client_server-py/hangman-client.py
+++ client_server-py/hangman-client.py
@@ -1,6 +1,5 @@
 ###############################################################################
-# client-python.py
-# Name: Alex Hoerr
+# hangman-client-python.py
 ###############################################################################
 
 import sys
@@ -9,8 +8,10 @@ import random
 import string
 
 # Size of chunks the client sends messages to the server with
+RECV_BUFFER_SIZE = 2048
 SEND_BUFFER_SIZE = 2048
 # client(): Open socket and send message from sys.stdin
+
 def client(server_ip, server_port):
 
     # Opens a socket and sets the socket mode to IPV4 and TCP
@@ -23,9 +24,11 @@ def client(server_ip, server_port):
         with open(0,'rb'):
             while True:
                 msg = sys.stdin.buffer.raw.read(SEND_BUFFER_SIZE)
-                if not msg: 
-                    return
                 sendmsg = sock.sendall(msg)
+                
+                data = sock.recv(RECV_BUFFER_SIZE)
+                sys.stdout.buffer.raw.write(data)
+            sys.stdout.flush()
 
 
 def main():
@@ -35,6 +38,28 @@ def main():
     server_ip = sys.argv[1]
     server_port = int(sys.argv[2])
     client(server_ip, server_port)
+
+# reads in data from the server
+def receive(socket):
+    value1 = int(socket.recv(1)[0])
+    if value1 == 0:
+        x, y = socket.recv(2)
+        return 0, socket.recv(int(x)), socket.recv(int(y))
+    else:
+        return 1, socket.recv(value1)
+ 
+# Game will take in and send user input; will also check if the guesses are valid
+def playHangman(s):
+    while True:
+        pkt = receive(s)
+        messageFlag = pkt[0]
+        if messageFlag != 0:
+            message = pkt[1].decode('utf8')
+            print(message)
+        else:
+            chosenWord = pkt[1].decode('utf8')
+            incorrectGuesses = pkt[2].decode('utf8')
+            
 
 if __name__ == "__main__":
     main()
